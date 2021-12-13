60d35dc681a226a9aa60f46b5d55dcc17b44dd66
rafaelhrocha <rafaelehnrq@gmail.com>
2021-12-13 16:30:18 -0300

    add bounce animation

--- main.js
+++ main.js
@@ -3,3 +3,15 @@
 function redirect(page){
     document.getElementById(page).style.transform = "scale(1)"
 }
+
+
+const ball = document.querySelector(".area-ball");
+
+ball.addEventListener("animationend" , event => {
+    if (event.animationName == "bounce"){
+        ball.classList.add("treme");
+        document.querySelector(".button-active-light").classList.add("changeLightColor");
+        console.log("animation end - bounce");
+    }
+})
+
